11e34d0e433ab51ce899f3e2982caab0a6870645
maxjamb <maxwelljamesbeard@gmail.com>
2020-02-25 22:07:58 +0000

    added some semblance of styles, materialUI etc.

--- focaldata/src/components/surveys/Survey.js
+++ focaldata/src/components/surveys/Survey.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import * as actionCreators from "../state/actionCreators";
-import Card from "./Card";
-import { getData } from "../state/actionCreators";
+import * as actionCreators from "../../state/actionCreators";
+import Card from "../Card";
+import { getData } from "../../state/actionCreators";
 
 export function Survey({ data, getData }) {
   return (
